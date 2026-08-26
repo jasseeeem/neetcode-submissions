4d075f50f84fdf43355c0c380526dd6ae3df5287
Muhammed Jaseem Pallikkal <64106637+jasseeeem@users.noreply.github.com>
2026-08-27 02:04:23 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    static bool comparator(pair<int, int> a, pair<int, int> b) {
+        return a.first > b.first;
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> frequency_map;
+        for (int num: nums) {
+            frequency_map[num]++;
+        }
+
+        priority_queue<pair<int, int>> q;
+        for (const auto& [num, freq] : frequency_map) {
+            q.push({freq, num});
+        }
+
+        std::vector<int> output;
+        while(output.size() < k) {
+            output.push_back(q.top().second);
+            q.pop();
+        }
+        return output;
+    }
+};
